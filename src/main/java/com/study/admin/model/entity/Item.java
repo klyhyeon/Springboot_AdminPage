d46060342f350bf8ca24535c3202299ec69f11a2
yuhyeon <klyhyeon@gmail.com>
2021-07-31 14:42:17 +0900

    test: git fetch test

--- src/main/java/com/study/admin/model/entity/Item.java
+++ src/main/java/com/study/admin/model/entity/Item.java
@@ -50,6 +50,7 @@ public class Item {
      * left outer join user user1_ on orderdetai0_.user_id=user1_.id
      * where orderdetai0_.item_id=?
      * */
+    
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
     private List<OrderDetail> orderDetailList;
 }
